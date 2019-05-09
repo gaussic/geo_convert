1133ed9fddb1a7a24b03650690bf80a183cbe98d
gaussic <dzkangshu@hotmail.com>
2019-05-09 23:51:36 +0800

    add MapBar

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # geo_convert
-地图经纬度转换
+
+Geolocataion converting between WGS84, BD09 and GCJ02.
+
+WGS84 / BD09 / GCJ02 / MapBar 经纬度坐标互转。
+
+- WGS84: GPS coordinates for Google Earth (GPS坐标，谷歌地球使用)
+- GCJ02: national coordinate system developed by China (国测局坐标，谷歌中国地图、腾讯地图、高德地图使用)
+- BD09: Baidu coordinates (百度坐标系，百度地图使用)
+- MapBar: MapBar coordinates (图吧坐标系，图吧地图使用)
+
+Test website: http://gpsspg.com/maps.htm
--- converter.py
+++ converter.py
@@ -2,11 +2,12 @@
 
 """
 Geolocataion converting between WGS84, BD09 and GCJ02.
-WGS84 / BD09 / GCJ02 经纬度坐标互转。
+WGS84 / BD09 / GCJ02 / MapBar 经纬度坐标互转。
 
 - WGS84: GPS coordinates for Google Earth (GPS坐标，谷歌地球使用)
-- GCJ02: national coordinate system developed by China (国测局坐标，谷歌地图使用)
+- GCJ02: national coordinate system developed by China (国测局坐标，谷歌中国地图、腾讯地图、高德地图使用)
 - BD09: Baidu coordinates (百度坐标系，百度地图使用)
+- MapBar: MapBar coordinates (图吧坐标系，图吧地图使用)
 
 Test website: http://gpsspg.com/maps.htm
 
@@ -41,6 +42,8 @@ def gcj02_to_bd09(lng, lat):
 
 def gcj02_to_wgs84(lng, lat):
     """GCJ02 -> WGS84"""
+    if out_of_china(lng, lat):
+        return lng, lat
     dlat = transform_lat(lng - 105.0, lat - 35.0)
     dlng = transform_lng(lng - 105.0, lat - 35.0)
     radlat = lat / 180.0 * PI
@@ -55,6 +58,8 @@ def gcj02_to_wgs84(lng, lat):
 
 def wgs84_to_gcj02(lng, lat):
     """WGS84 -> GCJ02"""
+    if out_of_china(lng, lat):
+        return lng, lat
     dlat = transform_lat(lng - 105.0, lat - 35.0)
     dlng = transform_lng(lng - 105.0, lat - 35.0)
     radlat = lat / 180.0 * PI
@@ -67,17 +72,15 @@ def wgs84_to_gcj02(lng, lat):
     return lng, lat
 
 
-def bd09_to_wgs84(lng, lat):
-    """BD09 -> WGS84"""
-    lng, lat = bd09_to_gcj02(lng, lat)
-    lng, lat = gcj02_to_wgs84(lng, lat)
-    return lng, lat
-
-
-def wgs84_to_bd09(lng, lat):
-    """WGS84 -> BD09"""
-    lng, lat = wgs84_to_gcj02(lng, lat)
-    lng, lat = gcj02_to_bd09(lng, lat)
+def mapbar_to_wgs84(lng, lat):
+    """MapBar -> WGS84"""
+    lng = lng * 100000.0 % 36000000
+    lat = lat * 100000.0 % 36000000
+    lng1 = int(lng - math.cos(lat / 100000.0) * lng / 18000.0 - math.sin(lng / 100000.0) * lat / 9000.0) 
+    lat1 = int(lat - math.sin(lat / 100000.0) * lng / 18000.0 - math.cos(lng / 100000.0) * lat / 9000.0)
+    lng2 = int(lng - math.cos(lat1 / 100000.0) * lng1 / 18000.0 - math.sin(lng1 / 100000.0) * lat1 / 9000.0 + (1 if lng > 0 else -1))
+    lat2 = int(lat - math.sin(lat1 / 100000.0) * lng1 / 18000.0 - math.cos(lng1 / 100000.0) * lat1 / 9000.0 + (1 if lat > 0 else -1)) 
+    lng, lat = lng2 / 100000.0, lat2 / 100000.0
     return lng, lat
 
 
@@ -99,6 +102,43 @@ def transform_lng(lng, lat):
     return ret
 
 
+def out_of_china(lng, lat):
+    """No offset when coordinate out of China."""
+    if lng < 72.004 or lng > 137.8437:
+        return True
+    if lat < 0.8293 or lat > 55.8271:
+        return True
+    return False
+
+
+def bd09_to_wgs84(lng, lat):
+    """BD09 -> WGS84"""
+    lng, lat = bd09_to_gcj02(lng, lat)
+    lng, lat = gcj02_to_wgs84(lng, lat)
+    return lng, lat
+
+
+def wgs84_to_bd09(lng, lat):
+    """WGS84 -> BD09"""
+    lng, lat = wgs84_to_gcj02(lng, lat)
+    lng, lat = gcj02_to_bd09(lng, lat)
+    return lng, lat
+
+
+def mapbar_to_gcj02(lng, lat):
+    """MapBar -> GCJ02"""
+    lng, lat = mapbar_to_wgs84(lng, lat)
+    lng, lat = wgs84_to_gcj02(lng, lat)
+    return lng, lat
+
+
+def mapbar_to_bd09(lng, lat):
+    """MapBar -> BD09"""
+    lng, lat = mapbar_to_wgs84(lng, lat)
+    lng, lat = wgs84_to_bd09(lng, lat)
+    return lng, lat
+
+
 if __name__ == '__main__':
     blng, blat = 121.4681891220,31.1526609317
     print('BD09:', (blng, blat))
@@ -108,3 +148,8 @@ if __name__ == '__main__':
     print('WGS84:', (wlng, wlat))
     print('WGS84 -> GCJ02:', wgs84_to_gcj02(wlng, wlat))
     print('WGS84 -> BD09:', wgs84_to_bd09(wlng, wlat))
+    mblng, mblat = 121.4667323772, 31.1450420991
+    print('MapBar:', (mblng, mblat))
+    print('MapBar -> WGS84:', mapbar_to_wgs84(mblng, mblat))
+    print('MapBar -> GCJ02:', mapbar_to_gcj02(mblng, mblat))
+    print('MapBar -> BD09:', mapbar_to_bd09(mblng, mblat))
